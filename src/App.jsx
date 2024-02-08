fd945b3ae92643c7e88c35645b170cc3fd12629c
Kirat <harkirat96@gmail.com>
2024-02-08 10:24:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is => {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
